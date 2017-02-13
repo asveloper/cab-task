b43dcd55abdcada620c57602cd615e721e4f71fc
Arslan Nasir <arslan@mobylogix.com>
2017-02-13 16:10:12 +0500

    Client locations schema and collection added

---
+++ models/client_locations.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const
+  mongoose = require("mongoose"),
+  Schema = mongoose.Schema,
+  require('mongoose-long')(mongoose);
+
+const clientLocationsSchema = new Schema({
+  clientID: Schema.Types.ObjectId,
+  latitude: Schema.Types.Long,
+  longtitude: Schema.Types.Long,
+  timeStamp: {type: Date, default: Date.now}
+}, {collection: 'client_locations', versionKey: false, shardKey: { _id: true } });
+
+module.exports = mongoose.model("ClientLocations", clientLocationsSchema);
