13dccab140ac608563f58032017f3d9841038691
Arslan Nasir <arslan@mobylogix.com>
2017-02-13 17:58:20 +0500

    Ride tracking model updated

--- models/ride_tracking.js
+++ models/ride_tracking.js
@@ -9,6 +9,10 @@ const rideTrackingSchema = new Schema({
   driverId: Schema.Types.ObjectId,
   latitude: Schema.Types.Long,
   longtitude: Schema.Types.Long,
+  accuracy: Schema.Types.Long,
+  bearing: Schema.Types.Long,
+  speed: Schema.Types.Long,
+  altitude: Schema.Types.Long,
   timeStamp: {type: Date, default: Date.now}
 }, {collection: 'ride_trackings', versionKey: false, shardKey: { _id: true } });
 
