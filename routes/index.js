5bebff3cf48f67fa539eaa6aef616dbdadd26b1f
Arslan Nasir <arslan@mobylogix.com>
2017-02-16 15:52:46 +0500

    Routes updated

--- routes/index.js
+++ routes/index.js
@@ -3,22 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'Express' });
-  var socket = io.connect('http://localhost:3000');
-
-  var callCab = confirm("Do you want to call cab?");
-
-  if (callCab) {
-    socket.emit('call cab', { clientId: 'client_id_here' });
-  }
-
-  socket.on('on its way', function (data) {
-    console.log("on its way....");
-    console.log(data);
-  });
-
-  res.sendStatus(200);
-
+  res.render('index', { title: 'Express' });
 });
 
 router.get('/driver', function(req, res, next) {
--- routes/users.js
+++ routes/users.js
@@ -8,13 +8,16 @@ router.get('/generate_token', function(req, res, next) {
 
   var token = jwt.sign(new Date(), config.get('secret'));
 
-  var user = new User({
+  User.findOneAndUpdate({
+    email: "arslan@gmail.com",
+    password: "password",
+  }, {
     email: "arslan@gmail.com",
     password: "password",
     jwtAccessToken: token
+  }, {upsert: true, 'new': true}, function(err, res) {
+      if (err) console.log(err);
   });
-
-  user.save((err) => { });
 
   res.send({token: token});
 });
