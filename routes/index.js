e61d9a9a26929902a1ce3b8006fde2d94581a55e
Arslan Nasir <arslan@mobylogix.com>
2017-02-17 19:32:06 +0500

    Check if token is present

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,13 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Cab' });
-  var socket = require('socket.io-client')('http://localhost:3000', {query: "token="+req.query.token});
-  socket.emit("call cab");
-  res.sendStatus(200);
+  if (req.query.token) {
+    var socket = require('socket.io-client')('http://localhost:3000', {query: "token="+req.query.token});
+    socket.emit("call cab");
+    res.sendStatus(200);
+  } else {
+    res.sendStatus(400);
+  }
 });
 
 router.get('/driver', function(req, res, next) {
